0ed122c7a849a13a6fffd72bad8b4e17a3a116d2
Krupesh Anadkat <kmanadkat@gmail.com>
2018-04-18 22:28:02 +0530

    Major Update
    Hero , Transparent NavBar,
    Table color green,
    Responsive hero

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
-
+	
+// DEFAULT HOME POSITION NAVBAR SETTING
+	
+	$('.navbar').removeClass('navbar-bg-light');
+	$('.nav-link').addClass('bg-light-link');
+	$('.navbar-brand').addClass('bg-light-brand');
+	
+// SCROLLING EFFECT
+	
     $('body').plusAnchor({
         easing: 'easeInOutExpo',
         speed:  800
@@ -15,4 +23,23 @@ $(document).ready(function(){
 	$('.navbar a').click(function(){
 		$('.navbar-collapse').collapse('hide');
 	});
+	
+
+// NAVBAR TRANSITION ON SCROLLING
+
+        $(window).scroll(function() {
+          if($(this).scrollTop() > 350) { 
+              $('.navbar').addClass('navbar-bg-light');
+			  $('.nav-link').removeClass('bg-light-link');
+			  $('.navbar-brand').removeClass('bg-light-brand');
+          }
+		  if($(this).scrollTop() <= 350) {
+			  $('.navbar').removeClass('navbar-bg-light');
+			  $('.nav-link').addClass('bg-light-link');
+			  $('.navbar-brand').addClass('bg-light-brand');
+		  }
+        });
+	
 });
+
+        
